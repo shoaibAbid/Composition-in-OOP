db2dcb86b2583f906bdba1036f14f3f37a9dfa83
Shoaib Abid <73983203+shoaibAbid@users.noreply.github.com>
2023-11-15 10:59:06 +0500

    Update program.py

--- program.py
+++ program.py
@@ -1,23 +1,12 @@
 # In program.py
 
-import hr
-import employees
-import productivity
+from hr import PayrollSystem
+from productivity import ProductivitySystem
+from employees import EmployeeDatabase
 
-manager = employees.Manager(1, 'Mary Poppins', 3000)
-secretary = employees.Secretary(2, 'John Smith', 1500)
-sales_guy = employees.SalesPerson(3, 'Kevin Bacon', 1000, 250)
-factory_worker = employees.FactoryWorker(4, 'Jane Doe', 40, 15)
-temporary_secretary = employees.TemporarySecretary(5, 'Robin Williams', 40, 9)
-
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-    temporary_secretary,
-]
-productivity_system = productivity.ProductivitySystem()
+productivity_system = ProductivitySystem()
+payroll_system = PayrollSystem()
+employee_database = EmployeeDatabase()
+employees = employee_database.employees
 productivity_system.track(employees, 40)
-payroll_system = hr.PayrollSystem()
 payroll_system.calculate_payroll(employees)
