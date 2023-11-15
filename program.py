805f163c96c5ad9d4becc9c0cd9d99bc28ba8088
Shoaib Abid <73983203+shoaibAbid@users.noreply.github.com>
2023-11-15 11:01:38 +0500

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 # In program.py
 
-from hr import PayrollSystem
+from hr import PayrollSystem, HourlyPolicy
 from productivity import ProductivitySystem
 from employees import EmployeeDatabase
 
@@ -8,5 +8,8 @@ productivity_system = ProductivitySystem()
 payroll_system = PayrollSystem()
 employee_database = EmployeeDatabase()
 employees = employee_database.employees
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
+
 productivity_system.track(employees, 40)
 payroll_system.calculate_payroll(employees)
